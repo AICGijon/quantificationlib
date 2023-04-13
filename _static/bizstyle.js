c0049a7ebae90a530f25a3b04b5def5c8ab1248b
pglez82 <pglez82@gmail.com>
2023-04-13 14:21:29 +0000

    Deploying to gh-pages from @ AICGijon/quantificationlib@f918ab6372786ae109a8de05a0699782561be879 🚀

--- _static/bizstyle.js
+++ _static/bizstyle.js
@@ -23,7 +23,7 @@ const initialiseBizStyle = () => {
 }
 
 window.addEventListener("resize",
-  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "quantificationlib 0.0.6 documentation")
+  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "quantificationlib 0.0.7 documentation")
 )
 
 if (document.readyState !== "loading") initialiseBizStyle()
