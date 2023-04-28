562d92179fdb3db1efe614ea358446c6bff6b937
JAlonsoG <jalonso@uniovi.es>
2023-04-28 13:42:21 +0200

    Added boxplot function

--- quantificationlib/plot/qlplot.py
+++ quantificationlib/plot/qlplot.py
@@ -40,7 +40,7 @@ def _sort_rows_bycolumns(m, columns=[0], order='ascending'):
 #     m_new = m[:, idx_means]
 #     return m_new, idx_means
 
-def plot_line_prevalences(ax, prevalence_matrix, order=None, colors=list(mcolors.TABLEAU_COLORS)):
+def plot_line_prevalences(ax, prevalence_matrix, order=None, colors=None):
     """
     Function to build a graph to represent prevalences of experiments in a linear form.
     Each horizontal line corresponds to one experiment, each color is a class
@@ -54,17 +54,19 @@ def plot_line_prevalences(ax, prevalence_matrix, order=None, colors=list(mcolors
     prevalence_matrix : ndarray, shape(number_of_experiments, number_of_clases)
         Prevalences of the experiments
 
-    order: str    
+    order: str, optional    
         To sort matrix based on the first column in order "ascending", "desdending" or none
        
-    colors: list
-        List of colors for classes
+    colors: list, optional
+        List of colors for classes. If None list(mcolors.TABLEAU_COLORS) are used
 
     """
 
+    if colors == None:
+        colors = list(mcolors.TABLEAU_COLORS)
+
     if order is not None:
        prevalence_matrix = _sort_rows_bycolumns(prevalence_matrix, order=order)
-
 
     N = 100  # points per line
     n_rows = prevalence_matrix.shape[0] 
@@ -85,3 +87,51 @@ def plot_line_prevalences(ax, prevalence_matrix, order=None, colors=list(mcolors
            #print(segments)
            line_segments = LineCollection(segments, colors=colors, linewidths=2)
            ax.add_collection(line_segments)
+
+
+def plot_boxes(ax, error_matrix, vert=None, y_title=None, x_title=None,
+                labels=None, colors=None):
+    """
+    Function to build a boxplot to visually show the error distribution of experiments
+
+    Parameters
+    ----------
+    ax : Axes
+        The axes to draw to
+
+    error_matrix : ndarray, shape(number_of_experiments, number_of_systems)
+        Error values of the experiments
+
+    vert: bool, default: True    
+            If `True`, draws vertical boxes.
+            If `False`, draw horizontal boxes.
+
+    y_title: str, optional
+            y-axis title
+
+    x_title: str, optional
+            x-axis title           
+    
+    labels: list, optional
+            Label for each system
+
+    colors: list, optional 
+        List of colors for boxes. If None default colors are used
+
+    """
+
+    ax.set_xlabel(x_title)
+    ax.set_ylabel(y_title)
+
+    if colors == None:
+        bplot = ax.boxplot(error_matrix, notch=False, sym='b+', labels=labels, vert=vert)
+    else: 
+        bplot = ax.boxplot(error_matrix, notch=False, sym='b+', labels=labels, vert=vert,  patch_artist=True)
+
+        # fill with colors
+        for patch, color in zip(bplot['boxes'], colors):
+            patch.set_color(color)
+            patch.set_facecolor(color)
+        
+        for median in bplot['medians']:
+             median.set(color='black')
